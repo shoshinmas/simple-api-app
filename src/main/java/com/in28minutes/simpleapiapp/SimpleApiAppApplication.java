70c3f985bc1f8877b2873096a02f1dd3cd8d8fac
Marcin <shoshinmas@protonmail.com>
2022-09-14 14:54:13 +0200

    Simple api test in spring

---
+++ src/main/java/com/in28minutes/simpleapiapp/SimpleApiAppApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.simpleapiapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleApiAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleApiAppApplication.class, args);
+    }
+
+}
